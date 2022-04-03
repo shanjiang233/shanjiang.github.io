2f4085bc81f3783164a9dc31d3f11c98d1e9160b
shanshan_2333 <286655896@qq.com>
2022-04-03 14:59:03 +0800

    test2

--- sketch.js
+++ sketch.js
@@ -1,23 +1,41 @@
-var x = 200;
-var y = 0;
-var speed = 0;
-var gravity = 0.1;
+var canvas;
+var h1;
+var buttom;
+var bgcolor;
+var slider;
+var input;
+var name;
+
 
 function setup() {
-  createCanvas(400, 400);
+  canvas = createCanvas(400, 400);
+  canvas.position(200, 300);
+  bgcolor = color(20,110,220);
+  h1 = createElement("h1", "hhaha,this is rapstar");
+  h1.position(100, 300);
+  button = createButton("BUTTOn")
+  button.position(100,500);
+  button.mousePressed(changeColor);
+  slider = createSlider(10,500,50);
+  input = createInput('text');
+  name = createP('changing');
+
+}
+
+function changeColor(){
+  bgcolor = color(random(200)); 
+}
+
+function mousePressed() {
+  h1.html("I am here");
+  createP("my love where is " + random(-10, 10));
 }
 
 function draw() {
-  background(220);
-fill(30,25,30);
-  noStroke();
-  ellipse(x,y,20,20);
-  
-  y = y + speed;
-  speed = speed + gravity;
-  
-  if(y>height){
-    speed = speed * -0.9;
-    y = height;
-  }
+  //clear();
+  background(bgcolor);
+  ellipse(300,80,slider.value());
+  fill(20,240,10);
+  text(input.value(),20,210);
+  textSize(30);
 }
